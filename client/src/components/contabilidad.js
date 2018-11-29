d56306f24a5d9b4fb7027d62e2515dd927a26bd8
JoanAndoni <mcr_joan@hotmail.com>
2018-11-28 18:12:12 -0600

    wefe

--- client/src/components/contabilidad.js
+++ client/src/components/contabilidad.js
@@ -8,14 +8,13 @@ export default class Contabilidad extends Component {
     super(props);
 
     this.state = {
-      alumnos: [],
+      ingresos: 170000.7,
+      egresos: 10100.98,
+      amount: 0,
     };
   }
 
   render() {
-        var ingresos = 170000.70;
-        var egresos = 10100.98;
-        var balanceGeneral = ingresos-egresos;
     return (
       <div class="all">
       <NavigationAdmin/>
@@ -24,14 +23,14 @@ export default class Contabilidad extends Component {
         <div class="row justify-content-around">
           <div class="col-4 box">
             <h2 class="buttonBalance">Ingresos</h2>
-            <h5>{ingresos}</h5>
+            <h5>{this.state.ingresos}</h5>
             <input class="inputA" placeholder="Cantidad.."/>
             <br/>
             <a class="btn btn-outline-light buttonMargin">Aceptar</a>
           </div>
           <div class="col-4 box">
             <h2 class="buttonBalance">Egresos</h2>
-            <h5>{egresos}</h5>
+            <h5>{this.state.egresos}</h5>
             <input class="inputA" placeholder="Cantidad.."/>
             <br/>
             <a class="btn btn-outline-light buttonMargin">Aceptar</a>
@@ -39,8 +38,8 @@ export default class Contabilidad extends Component {
         </div>
         <div class="row justify-content-around">
           <div class="col-4 boxAlt margins">
-            <h4 class="buttonBalanceA" >Balance general</h4>
-            <h2>{balanceGeneral}</h2>
+            <h4 class="buttonBalanceA">Balance general</h4>
+            <h2>{this.state.ingresos - this.state.egresos}</h2>
           </div>
 
         </div>
